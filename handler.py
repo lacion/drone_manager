3121a2f0483800628c857993f1efd326bec3a6a0
Luis M. Morales S <luis.morales@locationlabs.com>
2015-08-13 22:59:32 +0200

    handler timeout

--- handler.py
+++ handler.py
@@ -1,6 +1,8 @@
 import time
 import logging
 import zmq
+import argparse
+import sys
 
 from manager import BaseManager
 
@@ -10,6 +12,7 @@ class Handler(BaseManager):
 
     def __init__(self, port, bind_address, handler_id):
         BaseManager.__init__(self, port, bind_address)
+        self.logger = logging.getLogger('Handler')
         self.id = handler_id
 
     def setup(self):
@@ -39,8 +42,16 @@ class Handler(BaseManager):
         if response["reply"]:
             self.logger.debug("received pong from manager")
 
+def check_arg(args=None):
+    parser = argparse.ArgumentParser(description='Drone handler service')
+    parser.add_argument('-M', '--manager_address',  help='Manager host:port', required='True', default='127.0.0.1:5000')
+
+    results = parser.parse_args(args)
+    return results.manager_address.split(":")
 
 if __name__ == '__main__':
+
+    manager_host = check_arg(sys.argv[1:])
 
     handler = Handler(5000, "127.0.0.1", 1)
     handler.setup()
--- manager.py
+++ manager.py
@@ -1,14 +1,18 @@
 import zmq
 import time
+import datetime
 import logging
+import argparse
+import sys
+
+from zmq.error import Again
 
 logging.basicConfig(level=logging.DEBUG, format='%(name)s: %(message)s')
 
 class BaseManager(object):
 
     def __init__(self, port="5000", bind_address="127.0.0.1"):
-        self.logger = logging.getLogger('HandlerManager')
-        self.logger.debug('__init__')
+        self.logger = logging.getLogger('BaseManager')
         self.port = port
         self.bind_address = bind_address
 
@@ -16,39 +20,75 @@ class BaseManager(object):
         self.socket = None
 
     def setup(self):
-        self.logger.debug('setup')
-        self.socket = zmq.Context().socket(zmq.REP)
+
+        self.socket = zmq.Context().socket(zmq.DEALER)
+        self.socket.setsockopt(zmq.IDENTITY, b"MANAGER")
+        self.socket.bind("tcp://{0}:{1}".format(self.bind_address, self.port))
+
+class HandlerWorker(object):
+
+    def __init__(self):
+        self.logger = logging.getLogger('HandleWorker')
+        self.handlers = []
+        self.timeout = 10
+
+    def setup(self):
+        self.socket = zmq.Context().socket(zmq.DEALER)
+        self.socket.setsockopt(zmq.IDENTITY, b"WORKER")
         self.socket.bind("tcp://{0}:{1}".format(self.bind_address, self.port))
 
     def read(self):
-        return self.socket.recv_json()
+        return self.socket.recv_json(zmq.NOBLOCK)
 
     def send_msg(self, payload):
         self.socket.send_json(payload)
 
-class HandlerManager(BaseManager):
+    def get_handlers_count(self):
+        return len(self.handlers)
 
-    def __init__(self, port, bind_address):
+    def search_handlers_by_id(self, id):
+        return next((item for item in self.handlers if item["id"] == id), False)
 
-        BaseManager.__init__(self, port, bind_address)
+    def remove_handler(self, id):
+        for i in reversed(range(len(self.handlers))):
+            if self.handlers[i].get('id') == id:
+                self.handlers.pop(i)
 
-        self.managers = []
+    def expire(self):
+        for handler in self.handlers:
+            diff = (datetime.datetime.now() - handler["time"]).seconds
+            if diff >= self.timeout:
+                self.remove_handler(handler["id"])
+                self.logger.debug("handler: {0}, expired".format(handler["id"]))
+                self.logger.info("no heartbeat from hander id={0} received for 10 seconds, dropping ({1} available handler)".format(handler["id"], self.get_handlers_count()))
+
+    def renew(self, id):
+        handler = self.search_handlers_by_id(id)
+
+        if handler:
+            handler["time"] = datetime.datetime.now()
+            self.logger.debug("handler: {0}, renewed".format(handler["id"]))
 
     def pong(self):
-        self.send_msg({"reply": True})
+        self.send_msg({b"reply": True})
 
     def handle(self):
-        message = self.read()
+        self.expire()
+        try:
+            message = self.read()
+            if message["msg_type"] == "register":
+                if message["id"] in self.handlers:
+                    self.send_msg({b"reply": False})
+                else:
+                    self.handlers.append({"id": message["id"], "time": datetime.datetime.now()})
+                    self.logger.info("handler with id={0} connected ({1} available handlers)".format(message["id"], self.get_handlers_count()))
+                    self.send_msg({b"reply": True})
 
-        if message["msg_type"] == "register":
-            if message["id"] in self.managers:
-                self.send_msg({"reply": False})
-            else:
-                self.managers.append(message["id"])
-                self.send_msg({"reply": True})
-
-        if message["msg_type"] == "ping":
-            self.pong()
+            if message["msg_type"] == "ping":
+                self.renew(message["id"])
+                self.pong()
+        except Again:
+            self.logger.debug("move along, nothing to see here.")
 
 
 class DroneManager(BaseManager):
@@ -62,10 +102,23 @@ class DroneManager(BaseManager):
     def handle(self):
         pass
 
+
+def check_arg(args=None):
+    parser = argparse.ArgumentParser(description='Drone manager service')
+    parser.add_argument('-H', '--listen_handlers',  help='Handlers host:port', required='True', default='127.0.0.1:5000')
+    parser.add_argument('-D', '--listen_drones',  help='Drones host:port', required='False', default='127.0.0.1:5001')
+
+    results = parser.parse_args(args)
+    return results.listen_handlers.split(":"), results.listen_drones.split(":")
+
+
 if __name__ == '__main__':
 
-    handler = HandlerManager(5000, "127.0.0.1")
+    handler_host, drones_host = check_arg(sys.argv[1:])
+
+    handler = HandlerManager(handler_host[1], handler_host[0])
     handler.setup()
+    handler.logger.info("manager started ({0} available handlers)".format(handler.get_handlers_count()))
 
     while True:
         handler.handle()
